e2c803f6967f2c0e0925c67aaa89d96865a03cbc
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-11-09 22:07:05 +0530

    Manual + Google Auth, DB connection

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const port = 5000;
 var cors = require('cors');
 app.use(express.json());
 app.use(cors());
-
+const Router = require("./routes/route");
 const connecttodb = require('./config/database'); 
 connecttodb();
+
+app.use('/api/auth', Router);
 
 app.listen(port,()=>{
     console.log("Listening to Port-",port);
