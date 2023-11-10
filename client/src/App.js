50399edca0a4ef081afa5377098b2b3ff2331d15
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-11-10 23:12:15 +0530

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
-
-import './App.css';
-
+import { Navbar } from "./Components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Signup } from "./Components/Signup";
+import { Login } from "./Components/Login";
+import { PersonalInfo } from "./Components/PersonalInfo";
+import { EditInfo } from "./Components/EditInfo";
+import { useState } from "react";
 function App() {
+  const [auth, setauth] = useState(null);
+  const [userdata, setuserdata] = useState({"name":"","email":"","photo":"","bio":"","phone":""});
+  const setdata = (user)=>{
+    setuserdata(user);
+  }
+  const fetchdata = ()=>{
+    return(userdata);
+  }
+  let client_id = "795036138117-hugj2ismoeil60kjpleafh4cja22uccv.apps.googleusercontent.com";
+  
   return (
-    <div className="App">
-      My app
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+        {/* <Route exact path = "/" element={<Navbar />} /> */}
+        <Route exact path = "/" element={<><Navbar name={userdata.name} photo={userdata.photo} /><PersonalInfo setdata={setdata} userdata = {userdata} /></>} />
+        <Route exact path = "/edit" element={<><Navbar /><EditInfo fetchdata = {fetchdata} /></>} />
+        <Route exact path = "/login" element={<Login client_id={client_id} setdata={setdata} setauth={setauth} />} />
+        <Route exact path = "/signup" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
